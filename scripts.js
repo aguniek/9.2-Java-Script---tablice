fd6652768f624c00517805cd39b8ea5ea8a3d6ad
Agnieszka Szczepkowska <agnieszka.szczepkowska.work@gmail.com>
2018-05-02 15:44:05 +0200

    additional tasks

--- scripts.js
+++ scripts.js
@@ -10,3 +10,33 @@ if (allNames.indexOf(newName) === -1) {
 }
 
 console.log(allNames);
+
+//wyfiltrować imiona zaczynającace sie na "K"
+
+var kopiaAllNames = allNames.slice(0);
+
+var namesStartedWithK = kopiaAllNames.filter(function (word) {
+    return word[0].toUpperCase() === "K";
+});
+
+console.log('Imiona zaczynające się na K:' + namesStartedWithK);
+
+//- imiona dłuższe niż 5 znakow
+
+var kopia2AllNames = allNames.slice(0);
+
+var namesLongerThan5Letters = kopia2AllNames.filter(function (name) {
+    return name.length > 5;
+});
+
+console.log('Imiona dłuższe niż 5 znakow:' + namesLongerThan5Letters);
+
+//- posortować je alfabetycznie
+
+var kopia3AllNames = allNames.slice(0);
+
+var createArrayAtoZ = kopia3AllNames.sort(function (name1, name2) {
+    return name1 > name2;
+});
+
+console.log('Imiona alfabetycznie:' + createArrayAtoZ);
